49ad622607b457e08627bbb00a4e4dd3d1add10b
Jaskirat0441 <singhjaskirat862@gmail.com>
2022-08-09 15:44:09 +0530

    <adding>

--- solution.js
+++ solution.js
@@ -2,5 +2,23 @@
 function major(arr)
 {
   //your code here
+  let count =1;
+  let prev= arr[0];
+
+  for(let i=0;i<arr.length;i++){
+    if(count==0){
+      count=1;
+      prev=arr[i];
+    }
+    if(prev==arr[i]){
+      count++;
+    }
+    else{
+      count--;
+    }
+  }
+
+  return prev;
+
 }
 module.exports=major;
